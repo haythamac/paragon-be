be60080e3c2bb359bda84c44b81d885fd87fbcc6
haythamac <janssen.uy.dev@gmail.com>
2026-01-02 00:10:17 +0800

    feat: fixed all the fetch api errors

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -14,7 +14,17 @@ class ItemsController extends Controller
 
     public function store(Request $request)
     {
-        $item = Items::create($request->all());
+        $validated = $request->validate([
+            'itemName' => 'required|string|max:255',
+            'rarity' => 'required|string',
+            'category' => 'required|exists:item_categories,id',
+        ]);
+
+        $item = Items::create([
+            'name' => $validated['itemName'],
+            'rarity' => $validated['rarity'],
+            'item_category_id' => $validated['category'],
+        ]);
         return response()->json([
             'success' => true,
             'data' => $item,
